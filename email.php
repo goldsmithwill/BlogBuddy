9accd832ab248e1be612854fcd6f4a71af73e6fb
goldsmithwill <goldsmith.will@gmail.com>
2016-06-19 10:29:23 -0400

    Split up view and edit, added empty checks

--- email.php
+++ email.php
@@ -10,42 +10,60 @@
 	<h2>BlogBuddy</h2>
 
 <?php
-$title = $_POST ['postTitle'];
-$category = $_POST ['postCategory'];
-$text = htmlspecialchars ( $_POST ['textArea'] );
-$subject = "New Blog Post: $title from the category $category";
 $dbc = mysqli_connect ( 'localhost', 'root', '', 'blogbuddy' ) or die ( 'Error connecting to MySQL server.' );
+
+$title;
+$category;
+$text;
+$subject;
 
 if (isset ( $_POST ['post'] )) {
 	
-	$query = "SELECT * FROM subscriptions";
-	
-	$result = mysqli_query ( $dbc, $query ) or die ( 'QUERY Error' );
-	
-	while ( $row = mysqli_fetch_array ( $result ) ) {
-		$to = $row ['email'];
-		$name = $row ['name'];
-		$msg = "Hi $name! Here is a new blog post: \n" . $text;
-		email ( $to, $subject, $msg );
-		echo 'Email sent to: ' . $to . '<br />';
+	if (empty ( $_POST ['postTitle'] ) || empty ( $_POST ['postCategory'] ) || empty ( htmlspecialchars ( $_POST ['textArea'] ) )) {
+		echo "Please go back and fill out every field.";
+	} else {
+		$title = $_POST ['postTitle'];
+		$category = $_POST ['postCategory'];
+		$text = htmlspecialchars ( $_POST ['textArea'] );
+		$subject = "New Blog Post: $title from the category $category";
+		
+		$query = "SELECT * FROM subscriptions";
+		
+		$result = mysqli_query ( $dbc, $query ) or die ( 'QUERY Error' );
+		
+		while ( $row = mysqli_fetch_array ( $result ) ) {
+			$to = $row ['email'];
+			$name = $row ['name'];
+			$msg = "Hi $name! Here is a new blog post: \n" . $text;
+			email ( $to, $subject, $msg );
+			echo 'Email sent to: ' . $to . '<br />';
+		}
+		
+		// echo (time ());
+		$query = "INSERT INTO posts VALUES(CURRENT_TIMESTAMP, '$category', '$title', '$text')";
+		$result = mysqli_query ( $dbc, $query ) or die ( 'QUERY Error' );
 	}
-	
-	// echo (time ());
-	$query = "INSERT INTO posts VALUES(CURRENT_TIMESTAMP, '$category', '$title', '$text')";
-	$result = mysqli_query ( $dbc, $query ) or die ( 'QUERY Error' );
 } else if (isset ( $_POST ['share'] )) {
 	$to = $_POST ['toEmail'];
 	$from = $_POST ['fromEmail'];
 	$msg = "Hi $to! Here is a new blog post that $from has shared with you: \n" . $text;
 	email ( $to, $subject, $msg );
-} else if (isset ( $_POST ['subscribe'] )) {
-	
-	$name = $_POST ['subName'];
-	$email = $_POST ['subEmail'];
-	
-	$query = "INSERT INTO subscriptions VALUES('$name', '$email')";
-	
-	$result = mysqli_query ( $dbc, $query ) or die ( 'QUERY Error' );
+} else if (isset ( $_POST ['subscribe'] ) || isset ( $_POST ['unsubscribe'] )) {
+	if (empty ( $_POST ['subName'] ) || empty ( $_POST ['subEmail'] )) {
+		echo "Please go back and fill out every subscribe/unsubscribe field.";
+	} else {
+		$query;
+		$name = $_POST ['subName'];
+		$email = $_POST ['subEmail'];
+		
+		if (isset ( $_POST ['subscribe'] )) {
+			$query = "INSERT INTO subscriptions VALUES('$name', '$email')";
+		} else if (isset ( $_POST ['unsubscribe'] )) {
+			$query = "DELETE FROM subscriptions WHERE name='$name' AND email='$email'";
+		}
+		
+		$result = mysqli_query ( $dbc, $query ) or die ( 'QUERY Error' );
+	}
 }
 
 mysqli_close ( $dbc );
